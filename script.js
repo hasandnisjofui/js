502f87166cd33324ba64ea4130a80fe67de1f3f9
hasandnisjofui <miralievhasan347@gmail.com>
2024-02-25 11:05:24 +0500

    js 2

---
+++ script.js
@@ -0,0 +1,7 @@
+let b=prompt(" string ga tegishli malumot")
+let c=+prompt("inferior typedagi malumot kiriting")
+
+let a=( Boolean( prompt( "Boolean typedagi malumot kiriting") ) )
+alert(`siz kiritgan ${b} sozi ${typeof (a)} turiga kiradi
+siz kiritgan ${c} sozi ${typeof (c)} turiga kiradi
+siz kirritgan ${a} sozi ${typeof (c)} turiga kiradi`)
